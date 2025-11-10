ba4c5c71fdb0029a11f0b9f1bf2c4772c103b935
AloukiUwU <114002965+AloukiUwU@users.noreply.github.com>
2025-11-10 12:42:28 +0100

    add: SceneManager with its functions + MainMenu Start

--- Assets/Scripts/UI Navigation/SceneNavManager.cs
+++ Assets/Scripts/UI Navigation/SceneNavManager.cs
@@ -1,4 +1,7 @@
+using NUnit;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum ENavigationScene
 {
@@ -14,7 +17,8 @@ public class SceneNavManager : Singleton<SceneNavManager>
 {
     #region Public Fields
 
-
+    public CanvasGroup canvasGroup;
+    public float duration;
 
     #endregion
 
@@ -26,18 +30,66 @@ public class SceneNavManager : Singleton<SceneNavManager>
 
     #region MonoBehaviour Callbacks
 
+    public override void Awake()
+    {
+        base.Awake();
+        if (canvasGroup == null) return;
 
+        StartCoroutine(EnterAnimation());
+    }
 
     #endregion
 
     #region Public Methods
 
-
+    public void LoadScene(ENavigationScene sceneID)
+    {
+        StartCoroutine(LeaveAnimation(sceneID));
+    }
 
     #endregion
 
     #region Private Methods
 
+    private IEnumerator EnterAnimation()
+    {
+        if (canvasGroup != null)
+        {
+            canvasGroup.gameObject.SetActive(true);
+            canvasGroup.alpha = 1f;
+
+            var time = Time.time;
+
+            while (Time.time - time < duration)
+            {
+                canvasGroup.alpha = 1 - ((Time.time - time) / duration);
+                yield return null;
+            }
+
+            canvasGroup.alpha = 0f;
+            canvasGroup.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator LeaveAnimation(ENavigationScene sceneID)
+    {
+        if (canvasGroup != null)
+        {
+            canvasGroup.gameObject.SetActive(true);
+            canvasGroup.alpha = 0f;
+
+            var time = Time.time;
+
+            while (Time.time - time < duration)
+            {
+                canvasGroup.alpha = (Time.time - time) / duration;
+                yield return null;
+            }
+
+            canvasGroup.alpha = 1f;
+            SceneManager.LoadScene((int) sceneID);
+        }
+    }
 
     #endregion
 }
